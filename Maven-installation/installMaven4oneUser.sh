6492b750ec8e43f81a52aa79bcc0009ef6aaa399
vally15boy <100036393+vally15boy@users.noreply.github.com>
2022-03-24 21:21:51 -0600

    Update installMaven4oneUser.sh

--- Maven-installation/installMaven4oneUser.sh
+++ Maven-installation/installMaven4oneUser.sh
@@ -8,10 +8,10 @@ sudo yum install wget nano tree unzip git-all -y
 sudo yum install java-11-openjdk-devel java-1.8.0-openjdk-devel -y
 #2. Install Maven.sh
 #Step1) Download the Maven Software
-sudo wget https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.zip
-sudo unzip apache-maven-3.8.4-bin.zip
-sudo rm -rf apache-maven-3.8.4-bin.zip
-sudo mv apache-maven-3.8.4/ maven
+sudo wget https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.4-bin.zip
+sudo unzip apache-maven-3.8.5-bin.zip
+sudo rm -rf apache-maven-3.8.5-bin.zip
+sudo mv apache-maven-3.8.5/ maven
 #Step3) Set Environmental Variable  -For Specific User eg ec2-user
 echo "export M2_HOME=/opt/maven" >>  ~/.bash_profile
 echo "export PATH=$PATH:$M2_HOME/bin" >> ~/.bash_profile
